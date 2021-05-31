15accaa09b45e41a85ddfcd12c2592d37354b2ad
Fu-Nan <1186174184@qq.com>
2021-05-31 14:28:36 +0800

    first one

--- services/app.py
+++ services/app.py
@@ -22,8 +22,9 @@ class AppService:
     def getAppAnalysis(self):
         # 根据下载次数和推荐指数降序
         dfApp = self.df.sort_values(by=["download", "vote"], ascending=False)
-        # 获取排名前10应用
+        # 获取排名前10应用,存储图表所需数据
         dfApp = dfApp.head(10)
+        # subplots可在一张图片绘制多个图表,fig表示整个图片,ax表示图片中各个图表
         fig, ax = plt.subplots(figsize=(20.4, 4.8))
         indexs = np.arange(len(dfApp["appName"]))
         ax.plot(indexs, dfApp["download"].values, label="下载数量")
